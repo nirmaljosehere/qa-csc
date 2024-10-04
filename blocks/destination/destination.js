f84354ef7e1040a63d7ca6bc92f0e38785b95ec0
Nirmal Jose <nirmalj@adobe.com>
2024-10-04 12:16:47 +0100

    destination block

--- blocks/destination/destination.js
+++ blocks/destination/destination.js
@@ -10,25 +10,23 @@ function getMetadata(name) {
 const aem = "https://publish-p135360-e1341441.adobeaemcloud.com";
 
 export default function decorate(block) {
-  console.log("block content page metadata variable >>>>>>>>>"+getMetadata("arrival"));
   const arrivalID = document.createElement('div');
   arrivalID.id = 'arrival';
-  console.log("block content arrival variable >>>>>>>>>"+block.querySelector('div:nth-child(1)').textContent.trim());
-  console.log("block content page metadata variable >>>>>>>>>"+getMetadata("arrival"));
   arrivalID.textContent =  block.querySelector('div:nth-child(1)').textContent.trim() || getMetadata("arrival");
   block.querySelector('div:nth-child(1)').replaceWith(arrivalID);
 
-  const cityDiv = document.createElement('div');
-  cityDiv.id = `city-${arrivalID.textContent}`;
-  block.querySelector('div:last-of-type').replaceWith(cityDiv);
+  // const cityDiv = document.createElement('div');
+  // cityDiv.id = `city-${arrivalID.textContent}`;
+  // block.querySelector('div:last-of-type').replaceWith(cityDiv);
+  const arrivalSlug = "london";
+  const departureSlug ="doha";
 
-  fetch(`${aem}/graphql/execute.json/qatar-airways/city-details-by-slug;slug=${arrivalID.textContent}`)
+  fetch(`${aem}/qatar-airways/get-arrival-departure-details;arrivalSlug={arrivalSlug};departureSlug={departureSlug}`)  
     .then(response => response.json())
     .then(response => {
-      const { cityName, cityDescription, contentBlocks } = response.data.cityList.items[0];
       console.log("response received>>>>>>>>>")
-      console.log(response.data.cityList.items[0]);
-      cityDiv.innerHTML = `<div class="nirmaljose">Test div</div>`;
+      console.log( response.data.arrivingInList.items[0]);
+      //cityDiv.innerHTML = `<div class="nirmaljose">Test div</div>`;
     })
     .catch(error => {
       console.error('Error fetching data:', error);
